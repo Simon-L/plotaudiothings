18274b17ad3d6bff9b539bc06394357283c6b525
Simon-L <lumis@xulepth.fr>
2023-07-07 18:11:44 +0200

    Now automatically parses data files from LTSpice!

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <regex>
 
 #include "sst/basic-blocks/modulators/ADSREnvelope.h"
 #include "digital.hpp"
@@ -12,18 +13,48 @@ using namespace sciplot;
 #include "chowdsp_visualizers/chowdsp_visualizers.h"
 #include <chowdsp_plugin_utils/chowdsp_plugin_utils.h>
 
-// #include "values_ch.hpp"
-#include "values_reso.hpp"
+// values oh ButterworthFilter 2 freq 7750 q 1.95
+// values ch ButterworthFilter 2 freq 11450 q 1.55
+// values ch output gain 2 FirstOrderHPF 8000 -> FirstOrderLPF 11000 ( -1500 <- 9500 -> +1500)
+// values oh output gain 1.3 FirstOrderHPF 9500 -> FirstOrderLPF 12500 ( -1500 <- 11000 -> +1500)
+// reso Resonator rfb 82e3 r_g 425 c 3.79e-9 gain 0.325
+
+// #include "bar.hpp"
+// #include "values_reso.hpp"
 #include "Resonator.hpp"
+
+void parseSimulationFile(juce::String simFilename, std::vector<float> *destXVec, std::vector<float> *destYVec)
+{
+    const std::regex txt_regex("([0-9.eE+-]+)	\\(([0-9.eE+-]+)dB,(.*)\\).*");
+    const juce::File simFile = juce::File(simFilename);
+    juce::StringArray linesArray;
+    simFile.readLines(linesArray);
+    // printf("Length linesArray %u\n", linesArray.size());
+    for (size_t i = 0; i < linesArray.size(); i++) {
+        if (i == 0)
+            continue;
+        std::smatch base_match;
+        std::string curLine = linesArray[i].toStdString();
+        if (std::regex_match(curLine, base_match, txt_regex))
+        {
+            // printf("%f\n", juce::String(base_match[1].str()).getFloatValue());
+            // printf("%f\n", juce::String(base_match[2].str()).getFloatValue());
+            destXVec->push_back(juce::String(base_match[1].str()).getFloatValue());
+            destYVec->push_back(juce::String(base_match[2].str()).getFloatValue());
+        }
+        // printf("%s -> %f %f\n", curLine.c_str(), juce::String(base_match[1].str()).getFloatValue(), juce::String(base_match[2].str()).getFloatValue());
+    }
+}
 
 auto main(int argc, const char *argv[]) -> int
 {   
-    
     const juce::File optionsFileJ = juce::File(argv[1]);
     chowdsp::GenericTweaksFile<false> options;
     options.initialise (optionsFileJ, 1);
     
     auto freq = options.getProperty<float> ("freq");
+    auto freq2 = options.getProperty<float> ("freq2");
+    auto freq3 = options.getProperty<float> ("freq3");
     auto q = options.getProperty<float> ("q");
     auto gain = options.getProperty<float> ("gain");
     auto xmin = options.getProperty<float> ("xmin");
@@ -33,56 +64,68 @@ auto main(int argc, const char *argv[]) -> int
     auto Rfb = options.getProperty<float> ("Rfb");
     auto R_g = options.getProperty<float> ("R_g");
     auto C = options.getProperty<float> ("C");
+    auto simFilename = options.getProperty<juce::String> ("file");
     
     printf("Options: %f %f %f\n", freq, q, gain);
     
-    chowdsp::ButterworthFilter< 3, chowdsp::ButterworthFilterType::Highpass, float> fi;
+    std::vector<float> freqValues;
+    std::vector<float> dbValues;
+    parseSimulationFile(simFilename, &freqValues, &dbValues);
+    
     HatResonatorWDF reso;
-    reso.prepare(48000);
-    // reso.setParameters(Rfb, R_g, C);
+    reso.prepare(96000);
+    reso.setParameters(Rfb, R_g, C);
     chowdsp::Gain<float> preGain;
     preGain.setGainLinear(gain);
     
+    chowdsp::FirstOrderHPF<float> fi;
+    chowdsp::FirstOrderLPF<float> fi2;
+    
+    fi.reset();
+    fi.calcCoefs(freq3 - 1500, 96000.0);
+    fi2.reset();
+    fi2.calcCoefs(freq3 + 1500, 96000.0);
     
     chowdsp::SpectrumPlotBase base {
             chowdsp::SpectrumPlotParams {
                 500.0f,
                 20000.0f,
-                -30.0f,
+                -20.0f,
                 30.0f }
     };
-    chowdsp::GenericFilterPlotter plotter { base, {} };
-    plotter.runFilterCallback = [&fi, &preGain, gain, freq, q, &reso] (const float* in, float* out, int N)
+    chowdsp::GenericFilterPlotter plotter { base, {96000.0f, 1.0f / 12.0f, 13} };
+    plotter.runFilterCallback = [&fi, &fi2, &preGain, gain, freq, freq2, q, &reso] (const float* in, float* out, int N)
     {   
         std::copy (in, in + N, out);
-        preGain.prepare({48000.0, N, 1});
+        preGain.prepare({96000.0, juce::uint32(N), 1});
         preGain.process (chowdsp::BufferView { out, N });
-        // fi.reset();
-        reso.reset();
-        reso.prepare(48000);
+        // reso.reset();
+        // reso.prepare(96000);
         // reso.setParameters(8000.0, 0.06);
-        // fi.prepare(1);
-        // fi.calcCoefs(freq, q, 48000.0);
-        // fi.processBlock (chowdsp::BufferView { out, N });
-        for (size_t i = 0; i < N; i++) {
-            out[i] = reso.processSample(out[i]);
-        }
+        // for (size_t i = 0; i < N; i++) {
+        //     out[i] = reso.processSample(out[i]);
+        // }
+        fi.prepare({96000.0, juce::uint32(N), 1});
+        fi.processBlock (chowdsp::BufferView { out, N });
+        fi2.prepare({96000.0, juce::uint32(N), 1});
+        fi2.processBlock (chowdsp::BufferView { out, N });
     };
 
-    const auto [freqAxis, magAxis] = plotter.plotFilterMagnitudeResponse();    
-    
+    auto [freqAxis, magAxis] = plotter.plotFilterMagnitudeResponse();
     Plot2D plot;
     plot.xlabel("Frequency (Hz)");
     plot.ylabel("Magnitude (dB)");
-    plot.drawCurve(freqAxis, magAxis).label("chowdsp::ButterworthFilter");
-    plot.drawCurve(freqValues, dbValues).label("LTspice sim");
+    plot.drawCurve(freqAxis, magAxis).label("");
+    plot.drawCurve(freqValues, dbValues).label("LTSpice");
+    // plot.drawCurve(freqValues, dbValues).label("B");
     plot.xtics().logscale();
     plot.xrange(xmin, xmax);
     plot.yrange(ymin, ymax);
     Figure fig = {{plot}};
     Canvas canvas = {{fig}};
     canvas.size(1300,1300);
-    canvas.show();
+    // canvas.show();
+    canvas.save("/tmp/plot.png");
     
     return 0;
 }
