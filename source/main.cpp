894a7f44adc2c1fd074f0a293b89f6896a4b2384
Simon-L <lumis@xulepth.fr>
2023-07-09 21:13:17 +0200

    Update code and options.json for closed hat adsr

--- source/main.cpp
+++ source/main.cpp
@@ -23,9 +23,32 @@ using namespace sciplot;
 // #include "values_reso.hpp"
 #include "Resonator.hpp"
 
-void parseSimulationFile(juce::String simFilename, std::vector<float> *destXVec, std::vector<float> *destYVec)
+void parseResponseSimulationFile(juce::String simFilename, std::vector<float> *destXVec, std::vector<float> *destYVec)
 {
     const std::regex txt_regex("([0-9.eE+-]+)	\\(([0-9.eE+-]+)dB,(.*)\\).*");
+    const juce::File simFile = juce::File(simFilename);
+    juce::StringArray linesArray;
+    simFile.readLines(linesArray);
+    // printf("Length linesArray %u\n", linesArray.size());
+    for (size_t i = 0; i < linesArray.size(); i++) {
+        if (i == 0)
+            continue;
+        std::smatch base_match;
+        std::string curLine = linesArray[i].toStdString();
+        if (std::regex_match(curLine, base_match, txt_regex))
+        {
+            // printf("%f\n", juce::String(base_match[1].str()).getFloatValue());
+            // printf("%f\n", juce::String(base_match[2].str()).getFloatValue());
+            destXVec->push_back(juce::String(base_match[1].str()).getFloatValue());
+            destYVec->push_back(juce::String(base_match[2].str()).getFloatValue());
+        }
+        // printf("%s -> %f %f\n", curLine.c_str(), juce::String(base_match[1].str()).getFloatValue(), juce::String(base_match[2].str()).getFloatValue());
+    }
+}
+
+void parseDCSimulationFile(juce::String simFilename, std::vector<float> *destXVec, std::vector<float> *destYVec)
+{
+    const std::regex txt_regex("([0-9.eE+-]+)	([0-9.eE+-]+).*");
     const juce::File simFile = juce::File(simFilename);
     juce::StringArray linesArray;
     simFile.readLines(linesArray);
@@ -67,7 +90,7 @@ void plotResponse()
 
     std::vector<float> freqValues;
     std::vector<float> dbValues;
-    parseSimulationFile(simFilename, &freqValues, &dbValues);
+    parseResponseSimulationFile(simFilename, &freqValues, &dbValues);
 
     HatResonatorWDF reso;
     reso.prepare(96000);
@@ -130,10 +153,14 @@ struct EnvTime
     static constexpr float defaultEtMin{-8}, defaultEtMax{3.32192809489}; // log2(10)
     float etMin{defaultEtMin}, etMax{defaultEtMax};
     
+    EnvTime() {}
+    EnvTime(float etMin, float etMax)
+        : etMin(etMin), etMax(etMax) {}
+    
     float value{etMin};
 
     float getValue() { return value; }
-    float setValue(float nvalue) { value = nvalue; }
+    void setValue(float nvalue) { value = nvalue; }
     
     std::string getDisplayValueString()
     {
@@ -178,12 +205,18 @@ struct EnvTime
     virtual bool getMinString(std::string &s) { return false; }
 };
 
-static constexpr int tbs{16};
+static constexpr int tbs{8};
 struct SRProvider
 {
     double samplerate{96000}, sampleRateInv{1.f / samplerate};
     float envelope_rate_linear_nowrap(float f) const { return tbs * sampleRateInv * pow(2.f, -f); }
 } srp;
+
+struct ShortRange
+{
+    // 100µs -> 4s
+    static constexpr float etMin{-13.287}, etMax{2.0};
+};
 
 void plotEnvelope()
 {   
@@ -191,18 +224,26 @@ void plotEnvelope()
     auto xmax = options.getProperty<float> ("xmax");
     auto ymin = options.getProperty<float> ("ymin");
     auto ymax = options.getProperty<float> ("ymax");
+    auto simFilename = options.getProperty<juce::String> ("file");
+    auto tEnvA = options.getProperty<float> ("tEnvA");
+    auto tEnvR = options.getProperty<float> ("tEnvR");
+    auto mult = options.getProperty<float> ("mult");
+    auto trigDuration = options.getProperty<float> ("trigDuration");
     
     rack::dsp::PulseGenerator trigger;
     
-    EnvTime et1;
+    EnvTime et1(ShortRange::etMin, ShortRange::etMax);
     
-    et1.setDisplayValueString(std::string("0.1 s"));
-    printf("%s\n", et1.getDisplayValueString().c_str());
+    // et1.setDisplayValueString(std::string("0.1 s"));
+    // printf("%s\n", et1.getDisplayValueString().c_str());
     
-    auto adsr = sst::basic_blocks::modulators::ADSREnvelope<SRProvider, tbs>(&srp);
+    et1.printDisplayValueString(0.0);
+    et1.printDisplayValueString(1.0);
+    
+    auto adsr = sst::basic_blocks::modulators::ADSREnvelope<SRProvider, tbs, ShortRange>(&srp);
     
     float deltaTime = 1./96000.;
-    float length = 1.0;
+    float length = 0.2;
     
     std::vector<float> adsr_vec;
     std::vector<float> time_vec;
@@ -210,29 +251,36 @@ void plotEnvelope()
     // std::vector<float> out3;
     // std::vector<float> time_x;
     
-    float EnvA = et1.getValueTime(0.2);
+    float EnvA = et1.getValueTime(tEnvA);
     float EnvD = et1.getValueTime(0.2);
-    float EnvS = 0.5;
-    float EnvR = et1.getValueTime(0.2);
+    float EnvS = 1.0;
+    float EnvR = et1.getValueTime(tEnvR);
     
-    trigger.trigger(0.5);
-    adsr.attackFrom(0.0, EnvA, 1, false); // initial, attacktime, ashp, digital?
     
     float time = 0.0f;
     while (time < length) {
+        if (time == int(0.001 / deltaTime) * deltaTime)
+        {
+            trigger.trigger(trigDuration);
+            adsr.attackFrom(0.0, EnvA, 1, false); // initial, attacktime, ashp, digital?
+        }
         auto b = trigger.process(deltaTime);
         adsr.process(EnvA, EnvD, EnvS, EnvR, 1, 1, 1, b); // a, d, s, r, ashp, dshp, rshp, gateActive
-        adsr_vec.push_back(adsr.output);
+        adsr_vec.push_back(adsr.output * mult);
         time_vec.push_back(time);
         
         time += deltaTime;
     }
     
+    std::vector<float> timeValues;
+    std::vector<float> voltageValues;
+    parseDCSimulationFile(simFilename, &timeValues, &voltageValues);
     
     Plot2D plot;
     plot.xlabel("Time (s)");
     plot.ylabel("Value");
     plot.drawCurve(time_vec, adsr_vec).label("ADSR");
+    plot.drawCurve(timeValues, voltageValues).label("LTSpice");
     plot.xrange(xmin, xmax);
     plot.yrange(ymin, ymax);
     Figure fig = {{plot}};
